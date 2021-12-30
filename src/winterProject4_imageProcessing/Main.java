8a4e250d6d4f90c1f5edba7a5feefb9b6b7cc200
leeejjju <dlwlswn02027@gmail.com>
2021-12-30 21:33:28 +0900

    init project

---
+++ src/winterProject4_imageProcessing/Main.java
@@ -0,0 +1,17 @@
+package winterProject4_imageProcessing;
+
+public class Main {
+
+	/*
+	 *정보
+	 2021-12-30-17:00~ 2021-01-03-11:00
+	 2021-winter 과제4, 간단한 이미지 편집 프로그램 만들기 
+	 HGU 전산전자공학부_ 22100579 이진주
+	 */
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
